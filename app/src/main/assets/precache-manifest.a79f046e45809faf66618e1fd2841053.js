2c5a406de0df93635aad3c41195d062cd602f09e
Marcos <100097898+Socram-Occots@users.noreply.github.com>
2025-03-07 16:17:04 -0600

    Implemented Tailwind

---
+++ app/src/main/assets/precache-manifest.a79f046e45809faf66618e1fd2841053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d784f4d0400f887c3b92315d375fa38f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0db7069788dcb4523b6",
+    "url": "./static/css/main.3f3da6c6.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "./static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "./static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "b0db7069788dcb4523b6",
+    "url": "./static/js/main.dfde319c.chunk.js"
+  },
+  {
+    "revision": "f71ade6712ff57ab04c1",
+    "url": "./static/js/runtime~main.98280e6f.js"
+  }
+]);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,21 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <img src="/logo192.png" alt="Logo" className="w-32 h-32 mb-6" />
+      <input type="text" placeholder="Enter something..." className="border p-2 w-64 mb-4 rounded" />
+      <button className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600">
+        Submit
+      </button>
     </div>
   );
 }
